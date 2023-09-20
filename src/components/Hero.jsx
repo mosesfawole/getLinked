d011a19e2254fbd2582b23e86dde314daba79fae
mosesfawole <mosesfawole2001@gmail.com>
2023-09-20 04:37:44 +0100

    initial hero component, another sigh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const Hero = () => {
   const { hero } = data;
   return (
-    <div className="hero flex flex-col gap-6 p-4">
+    <div className="hero flex flex-col gap-6 p-4 md:px-14">
       <div className="top flex items-center justify-center md:justify-end md:px-11 ">
         <p className="font-body font-bold text-[16px] leading-5 md:text-4xl italic  ">
           Igniting a Revolution in HR Innovation
@@ -22,42 +22,55 @@ const Hero = () => {
                   alt="purple"
                 />
               </div>
-              <div className="relative">
-                <span>
+              <div className="relative md:flex flex-col ">
+                <span className="flex justify-end">
                   <img
+                    className="md:w-[53px] h-[73px]"
                     width={18}
                     height={26}
                     src={hero.creative}
                     alt="creative"
                   />
                 </span>
-                <p className=" text-3xl font-bold md:">{hero.titleTop}</p>
-                <p className="flex gap-1 text-3xl font-bold md:">
+                <p className=" text-3xl font-bold md:text-[80px] md:leadng-[98px] md:mb-16">
+                  {hero.titleTop}
+                </p>
+                <p className="flex gap-1 text-3xl font-bold md:text-[80px] md:leadng-[98px] md:mb-8">
                   {`${hero.titleDown}`}{" "}
                   <span className="text-common"> 1.0</span>{" "}
                   <span className="flex">
-                    <img width={32.8} src={hero.chain} alt="chain" />{" "}
-                    <img width={22.12} src={hero.fire} alt="spark" />
+                    <img
+                      className="md:w-[86px]"
+                      width={32.8}
+                      src={hero.chain}
+                      alt="chain"
+                    />{" "}
+                    <img
+                      className="md:w-[56px]"
+                      width={22.12}
+                      src={hero.fire}
+                      alt="spark"
+                    />
                   </span>{" "}
                 </p>{" "}
               </div>
             </div>
             <div className="text flex mt-2 mb-4 ">
-              <p className="font-body px-10 font-regular text-[13px] text-start">
+              <p className="font-body px-10 font-regular text-[13px] md:text-xl text-start md:px-0">
                 {hero.text}
               </p>
             </div>
-            <div className="button mt-2 mb-4">
+            <div className="button mt-2 mb-4 md:mb-14">
               <Button />
             </div>
             <div className="countdown flex gap-4 ">
-              <p className="font-unica text-5xl">
+              <p className="font-unica text-5xl md:text-[64px] md:leading-[85px]">
                 00<span className="font-body text-xs">H</span>
               </p>
-              <p className="font-unica text-5xl">
+              <p className="font-unica text-5xl md:text-[64px] md:leading-[85px]">
                 00<span className="font-body text-xs">M</span>
               </p>
-              <p className="font-unica text-5xl">
+              <p className="font-unica text-5xl md:text-[64px] md:leading-[85px]">
                 00<span className="font-body text-xs">S</span>
               </p>
             </div>
